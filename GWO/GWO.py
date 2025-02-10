cbbe0f2aa0ba96abbd13341f0e53ddf9dee60c44
Khanel <m.khanloo.shendy@gmail.com>
2025-02-10 21:38:25 +0330

    feat(gwo): Add initial GWO module  Create base GrayWolfOptimization class with an empty optimize method. refactor(tsp): Separate TSP

---
+++ GWO/GWO.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Generic Gray Wolf Optimization (GWO) Module.
+
+This module provides a base for a generic and reusable GWO algorithm.
+"""
+
+class GrayWolfOptimization:
+    def __init__(self, problem):
+        self.problem = problem
+
+    def optimize(self):
+        pass
